2fccad79877c8009f43e288ac35413420283b133
John Dewey <john@dewey.ws>
2014-02-11 16:52:03 -0800

    Cleanups to whip back into shape

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name             "sysstat"
-maintainer       "John Dewey"
-maintainer_email "john@dewey.ws"
-license          "Apache 2.0"
-description      "Installs/Configures sysstat"
+name             'sysstat'
+maintainer       'John Dewey'
+maintainer_email 'john@dewey.ws'
+license          'Apache 2.0'
+description      'Installs/Configures sysstat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.3.0"
+version          '1.3.0'
 
-recipe           "sysstat", "Installs/Configures sysstat"
+recipe           'sysstat', 'Installs/Configures sysstat'
 
 %w{ debian ubuntu }.each do |os|
   supports os
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,57 +1,60 @@
-require "spec_helper"
+# encoding: UTF-8
 
-describe "sysstat::default" do
-  describe "ubuntu" do
+require_relative 'spec_helper'
+
+describe 'sysstat::default' do
+  describe 'ubuntu' do
     before do
-      @chef_run = ::ChefSpec::ChefRunner.new ::UBUNTU_OPTS do |n|
+      @chef_run = ChefSpec::Runner.new UBUNTU_OPTS do |n|
         n.set['sysstat']['sa1_options'] = '-S DISK -S INT'
         n.set['sysstat']['sa2_options'] = '-A'
-      end.converge "sysstat::default"
+      end.converge 'sysstat::default'
 
-      @file = @chef_run.template "/etc/default/sysstat"
+      @file = @chef_run.template '/etc/default/sysstat'
     end
 
-    it "installs package" do
-      expect(@chef_run).to upgrade_package "sysstat"
+    it 'installs package' do
+      expect(@chef_run).to upgrade_package 'sysstat'
     end
 
-    it "starts service" do
-      expect(@chef_run).to start_service "sysstat"
+    it 'starts service' do
+      expect(@chef_run).to start_service 'sysstat'
     end
 
-    it "enables service" do
-      expect(@chef_run).to set_service_to_start_on_boot "sysstat"
+    it 'enables service' do
+      expect(@chef_run).to enable_service 'sysstat'
     end
 
-    it "has proper owner" do
-      expect(@file).to be_owned_by("root", "root")
+    it 'has proper owner' do
+      expect(@file.owner).to eq('root')
+      expect(@file.group).to eq('root')
     end
 
-    it "has proper modes" do
-      expect(sprintf("%o", @file.mode)).to eq "644"
+    it 'has proper modes' do
+      expect(sprintf('%o', @file.mode)).to eq '644'
     end
 
-    it "is enabled" do
-      expect(@chef_run).to create_file_with_content @file.name,
-        %Q{ENABLED="true"}
+    it 'is enabled' do
+      expect(@chef_run).to render_file(@file.name)
+        .with_content %Q{ENABLED="true"}
     end
 
-    it "overrides sa1_options" do
-      expect(@chef_run).to create_file_with_content @file.name,
-        %Q{SA1_OPTIONS="-S DISK -S INT"}
+    it 'overrides sa1_options' do
+      expect(@chef_run).to render_file(@file.name)
+        .with_content %Q{SA1_OPTIONS="-S DISK -S INT"}
     end
 
-    it "overrides sa2_options" do
-      expect(@chef_run).to create_file_with_content @file.name,
-        %Q{SA2_OPTIONS="-A"}
+    it 'overrides sa2_options' do
+      expect(@chef_run).to render_file(@file.name)
+        .with_content %Q{SA2_OPTIONS="-A"}
     end
   end
 
-  describe "redhat" do
+  describe 'redhat' do
     before do
-      @chef_run = ::ChefSpec::ChefRunner.new ::REDHAT_OPTS
-      @chef_run.converge "sysstat::default"
-      @file = @chef_run.template "/etc/default/sysstat"
+      @chef_run = ChefSpec::Runner.new REDHAT_OPTS
+      @chef_run.converge 'sysstat::default'
+      @file = @chef_run.template '/etc/default/sysstat'
     end
 
     it "doesn't create file" do
